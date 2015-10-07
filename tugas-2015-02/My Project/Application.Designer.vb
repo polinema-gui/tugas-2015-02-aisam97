39f62538ea1b92cdc6ccddb9d0be77023b7ce75c
Muhammad Aisamuddin Eka Pramudita <isam.muhammad@ymail.com>
2015-10-07 18:37:21 +0700

    Revert "TUGAS 2"
    This reverts commit b385193e79ddb45f47944370cdb64c268b9256ad.

--- tugas-2015-02/My Project/Application.Designer.vb
+++ tugas-2015-02/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Kalkulator.Kalkulator
+            Me.MainForm = Global.Kalkulator.Form1
         End Sub
     End Class
 End Namespace
